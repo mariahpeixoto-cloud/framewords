6288c7436aa044ef1bdab7c67be751c73ff994e7
mariahpeixoto-cloud <mariah.peixoto@estudante.ifms.edu.br>
2026-05-20 12:12:22 -0400

    atualizaçãooooo

--- BACKEND/src/auth/use-cases/auth.service.ts
+++ BACKEND/src/auth/use-cases/auth.service.ts
@@ -1,1 +1,21 @@
-//AA
+import { Injectable } from "@nestjs/common";
+import { RegisterDto } from "../dto/register.dto";
+import { RegisterUseCase } from "./register.use-case";
+import { LoginDto } from "src/auth/dto/login.dto";
+import { LoginUseCases } from "./login.use-cases";
+
+@Injectable()
+    export class AuthService{
+        constructor( 
+            private readonly registerUseCases: RegisterUseCase,
+            private readonly loginUseCases: LoginUseCases,
+        ){}
+
+        async register(data: RegisterDto) {
+                return await this.registerUseCases.execute(data);
+            }
+        async login(data:LoginDto ) {
+            return await this.loginUseCases.execute(data);
+        }
+        
+    }
--- BACKEND/src/auth/use-cases/current-user.decorator.ts
+++ BACKEND/src/auth/use-cases/current-user.decorator.ts
@@ -1,1 +1,9 @@
-//AA
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user;
+  }
+);
